428c80b7691f98afac2dc513001f98919dbe76c0
Tristan Rasmussen <tristanisthebest@gmail.com>
2014-10-26 18:42:27 -0500

    Minor pylint changes.

--- webapp/deckr/urls.py
+++ webapp/deckr/urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
-urlpatterns = patterns('',
-    url(r'^$', 'deckr.views.index', name='deckr.index'),
-)
+INDEX = url(r'^$', 'deckr.views.index', name='deckr.index')
+urlpatterns = patterns('', INDEX)
