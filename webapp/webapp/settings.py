5b0633462a62306dd4dbc33985e1873af50b8c13
Tristan Rasmussen <tristanisthebest@gmail.com>
2014-10-26 18:04:51 -0500

    Added the nose runner to django app.

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'django_nose',
+    
     'deckr',
 )
 
@@ -83,3 +85,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
