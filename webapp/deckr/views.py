55219cad26fe76622c9189da9d8b7165d3075ca3
Tristan Rasmussen <tristanr93@gmail.com>
2014-10-30 12:17:21 -0500

    Minor alterations to the makefile.

--- webapp/deckr/views.py
+++ webapp/deckr/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-from deckr.sockets import ChatNamespace
-
+# We need to import the namespace so the URLs can be discovered.
+from deckr.sockets import ChatNamespace  # pylint: disable=unused-import
 
 def index(request):
     return render(request, "deckr/index.html", {})
