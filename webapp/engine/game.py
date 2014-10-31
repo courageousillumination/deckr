1bf64c21866ee56183305120d8644780f9d09211
Tristan Rasmussen <tristanr93@gmail.com>
2014-10-31 18:44:23 -0500

    Added some basic engine tests.

---
+++ webapp/engine/game.py
@@ -0,0 +1,11 @@
+class Game(object):
+    
+    def make_action(self, action_id, *args, **kwargs):
+        pass
+    
+    def set_up(self):
+        pass
+    
+    def is_over(self):
+        return False
+    
