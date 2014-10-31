dede61821c2a52ca04e1bfe606ba2187b6e08329
Shaan Sapra <ssapra@uchicago.edu>
2014-10-31 16:46:59 -0500

    Added player and game room validations and tests

--- webapp/deckr/urls.py
+++ webapp/deckr/urls.py
@@ -9,4 +9,4 @@ import socketio.sdjango
 INDEX = url(r'^$', 'deckr.views.index', name='deckr.index')
 SOCKETS = url(r'^socket\.io', include(socketio.sdjango.urls))
 
-urlpatterns = patterns('', SOCKETS, INDEX) # pylint: disable=C0103
+urlpatterns = patterns('', SOCKETS, INDEX)  # pylint: disable=C0103
