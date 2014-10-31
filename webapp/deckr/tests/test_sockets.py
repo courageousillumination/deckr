6fd69fab6cbdb3d8844287440ba65c7d22aeb7fc
Tristan Rasmussen <tristanisthebest@gmail.com>
2014-10-31 13:46:53 -0500

    Added some initial unittests and lettuce tests.

--- webapp/deckr/tests/test_sockets.py
+++ webapp/deckr/tests/test_sockets.py
@@ -1,5 +1,5 @@
 """
-Unit tests for deckr.
+Unit tests for deckr websockets.
 """
 
 from django.test import TestCase
@@ -7,78 +7,131 @@ from unittest import skip
 from socketio.virtsocket import Socket
 from mock import MagicMock
 
-from deckr.sockets import *
+from deckr.sockets import ChatNamespace, GameNamespace
+
 
 class MockSocketIOServer(object):
+
     """
     Mock a SocketIO server
     """
-    
+
     def __init__(self, *args, **kwargs):
+        super(MockSocketIOServer, self).__init__(*args, **kwargs)
         self.sockets = {}
 
     def get_socket(self, socket_id=''):
+        """
+        Simple look up in the local dictionary.
+        """
+
         return self.sockets.get(socket_id)
 
 
 class MockSocket(Socket):
+
+    """
+    Simple mocke socket.
+    """
+
     pass
 
-class ChatNamespaceTestCase(TestCase):
+
+class MockGameRunner(object):
+
+    """
+    Simple mock game runner.
+    """
+
+    pass
+
+
+class SocketTestCase(TestCase):
+
+    """
+    A simple abstract test case for any socket tests. This creates
+    a mock server and socket to make testing a little easier.
+    """
+
+    def setUp(self):
+        self.server = MockSocketIOServer()
+        self.environ = {}
+        self.socket = MockSocket(self.server, {})
+        self.socket.error = MagicMock()
+        self.environ['socketio'] = self.socket
+
+
+class ChatNamespaceTestCase(SocketTestCase):
+
     """
     Test the ChatNamespace for websockets.
     """
-    
+
     def setUp(self):
-        self.server = MockSocketIOServer()
-        self.environ = {}
-        self.socket = MockSocket(self.server, {})
-        self.socket.error = MagicMock()
-        self.environ['socketio'] = self.socket
+        super(ChatNamespaceTestCase, self).setUp()
+
         self.namespace = ChatNamespace(self.environ, '/chat')
         self.namespace.broadcast_event = MagicMock()
-        
+
     def test_chat(self):
+        """
+        Make sure that if we chat a message it is broadcast to
+        the entire namespace.
+        """
+
         message = "hello world"
-        
+
         self.namespace.on_chat(message)
-        
         self.namespace.broadcast_event.assert_called_with("chat", message)
-        
-class GameNamespaceTestCase(TestCase):
-    
+
+
+class GameNamespaceTestCase(SocketTestCase):
+
+    """
+    Test all the socket code around actually running the game.
+    """
+
     def setUp(self):
-        self.server = MockSocketIOServer()
-        self.environ = {}
-        self.socket = MockSocket(self.server, {})
-        self.socket.error = MagicMock()
-        self.environ['socketio'] = self.socket
+        super(GameNamespaceTestCase, self).setUp()
+
         self.namespace = GameNamespace(self.environ, '/game')
+        # Make sure we don't actually need to broadcast/emit events
         self.namespace.broadcast_event = MagicMock()
         self.namespace.emit = MagicMock()
-        
+        # Mock out the game runner
+        self.namespace.runner = MockGameRunner()
+
     @skip("Not yet implemented")
     def test_invalid_move(self):
+        """
+        If we send an invalid move we should get an error.
+        """
+
         invalid_move = "invalid"
+
         self.namespace.on_action(invalid_move)
-        
         self.namespace.emit.assert_called_with("error", "ERROR")
-        
+
     @skip("Not yet implemented")
     def test_valid_move(self):
+        """
+        If we send a valid move we should get a list of transitions.
+        """
+
         valid_move = "valid move"
-        transitions = "{ }"
-        
+        transitions = [{}]
+
         self.namespace.on_action(valid_move)
-        
         self.namespace.broadcast_event.assert_called_with("state_transition",
                                                           transitions)
-        
+
     @skip("Not yet implemented")
     def test_request_state(self):
-        state = "{ }"
-        
+        """
+        If we request the state, we should recieve the state back.
+        """
+
+        state = {'foo': 'bar'}
+
         self.namespace.on_request_state()
-        
         self.namespace.emit.assert_called_with("state", state)
-        
