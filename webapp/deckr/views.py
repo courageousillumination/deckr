0aaa7d366c95b57cd87bf694c798eb9adf6e465a
Graham Smith <grahamas@gmail.com>
2014-10-31 13:00:53 -0500

    Stupid typo in views.py.

--- webapp/deckr/views.py
+++ webapp/deckr/views.py
@@ -7,10 +7,6 @@ from django.shortcuts import render
 # We need to import the namespace so the URLs can be discovered.
 from deckr.sockets import ChatNamespace  # pylint: disable=unused-import
 
-from xml.dom import minidom
-
-DOC = minidom.parse("../../samples/solitaire/layout.html")
-
 def index(request):
     """
     Simply return the index page without any context.
