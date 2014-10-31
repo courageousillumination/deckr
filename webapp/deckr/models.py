c64e4bce77be60ff9cd5f3de36b2a0919db85640
Shaan Sapra <ssapra@uchicago.edu>
2014-10-31 15:51:49 -0500

    Fleshed out models for GameRoom and Player and added migrations

--- webapp/deckr/models.py
+++ webapp/deckr/models.py
@@ -5,10 +5,30 @@ Contains definitions of all models for deckr.
 from django.db import models
 
 
-class GameRoom(object):
-
+class GameRoom(models.Model):
     """
-    A simple wrapper around the deckr engine Game.
+    Contains attributes and methods for GameRoom class
     """
+    room_id = models.IntegerField()
+    max_players = models.IntegerField(default=8)
 
-    pass
+    def __unicode__(self):
+        """
+        Unicode representation of GameRoom object
+        """
+        return "Game Room for Game " + str(self.room_id)
+
+
+class Player(models.Model):
+    """
+    Contains attributes and methods for Player class
+    """
+    player_id = models.IntegerField()
+    game_room = models.ForeignKey(GameRoom)
+    nickname = models.CharField(max_length=128)
+
+    def __unicode__(self):
+        """
+        Unicode representation of Player class
+        """
+        return self.nickname
