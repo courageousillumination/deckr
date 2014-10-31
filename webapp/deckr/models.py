a0bbb3b5e1ed9070b159000d5a899513ecadd743
Tristan Rasmussen <tristanisthebest@gmail.com>
2014-10-31 14:05:56 -0500

    Minor changes to make sure tests pass.

--- webapp/deckr/models.py
+++ webapp/deckr/models.py
@@ -5,7 +5,7 @@ Contains definitions of all models for deckr.
 from django.db import models
 
 
-class GameRoom(models.Model):
+class GameRoom(object):
 
     """
     A simple wrapper around the deckr engine Game.
