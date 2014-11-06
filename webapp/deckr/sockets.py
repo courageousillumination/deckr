835f2a3d4b0ab9ab7977143bfc81dd9fe18cd591
Tristan Rasmussen <tristanr93@gmail.com>
2014-11-06 13:01:48 -0600

    Implementing some basic socket tests.

--- webapp/deckr/sockets.py
+++ webapp/deckr/sockets.py
@@ -2,9 +2,15 @@
 Stores all the socket logic for the deckr webapp.
 """
 
+from django.core.exceptions import ObjectDoesNotExist
+
 from socketio.namespace import BaseNamespace
 from socketio.mixins import RoomsMixin, BroadcastMixin
 from socketio.sdjango import namespace
+
+from engine import game_runner
+
+from deckr.models import Player, GameRoom
 
 
 @namespace('/chat')
@@ -33,7 +39,7 @@ class ChatNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
 
 
 @namespace('/game')
-class GameNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
+class GameNamespace(BaseNamespace, RoomsMixin):
 
     """
     Represents simple socket logic for a Chat Room. Whenever it
@@ -41,12 +47,68 @@ class GameNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
     channel.
     """
 
+    def __init__(self, *args, **kwargs):
+        super(GameNamespace, self).__init__(*args, **kwargs)
+
+        self.game_room = None
+        self.player = None
+        self.room = None
+
     def initialize(self):
         """
         Mainly for debug.
         """
 
         print "Got socket connection."
+        self.runner = game_runner
+        
+    def recv_disconnect(self):
+        """
+        When we disconnect make sure we clean up any related
+        objects.
+        """
+        
+        if self.player is not None:
+            self.player.delete()
+
+    def on_join(self, room):
+        """
+        Triggers when a client joins this room. Each room corresponds
+        to a GameRoom object. If this gets a bad ID, or it fails
+        to create it will return False and emit an error message.
+        """
+
+        try:
+            room_id = int(room)
+        except ValueError:
+            self.emit("error", "Room id is not an integer.")
+            return False
+
+        # Get the game room object
+        try:
+            game_room = GameRoom.objects.get(pk=int(room_id))
+        except ObjectDoesNotExist:
+            self.emit("error", "Can not find game room")
+            return False
+
+        # Attempt to create a player for this room
+        try:
+            player_id = self.runner.add_player(game_room.room_id)
+            player = Player.objects.create(game_room=game_room,
+                                           nickname="Bob",
+                                           player_id=player_id)
+        except ValueError:
+            self.emit("error", "Unable to join game room.")
+            return False
+
+        # Now that we've created a player we can actually
+        # join the room
+        self.player = player
+        self.game_room = game_room
+        self.room = room
+        self.join(room)
+
+        return True
 
     def on_make_action(self, data):
         """
@@ -56,4 +118,30 @@ class GameNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
 
         print "Got socket data", data
         self.emit('move_card', data)
-        #self.broadcast_event('chat', msg)
+        
+    def on_request_state(self, data):
+        """
+        The client will call this whenever they want the entire
+        game state.
+        """
+        
+        if self.game_room is None:
+            self.emit("error", "Please connect to a game room first.")
+            return False
+        
+        state = self.runner.get_state(self.game_room.room_id)
+        self.emit("state", state)
+        return True
+    
+    def flush(self):
+        """
+        Clear out all internal state. Should only be used
+        for testing.
+        """
+        
+        if self.player is not None:
+            self.player.delete()
+        
+        self.player = None
+        self.game_room = None
+        self.room = None
--- webapp/deckr/tests/test_sockets.py
+++ webapp/deckr/tests/test_sockets.py
@@ -8,7 +8,7 @@ from socketio.virtsocket import Socket
 from mock import MagicMock
 
 from deckr.sockets import ChatNamespace, GameNamespace
-from deckr.models import Player
+from deckr.models import Player, GameRoom
 
 
 class MockSocketIOServer(object):
@@ -101,6 +101,45 @@ class GameNamespaceTestCase(SocketTestCase):
         self.namespace.emit = MagicMock()
         # Mock out the game runner
         self.namespace.runner = MockGameRunner()
+        self.namespace.runner.add_player = MagicMock()
+        
+        self.game_room = GameRoom.objects.create(room_id=0,
+                                                 max_players=1)
+        
+        self.namespace.on_join(str(self.game_room.pk))
+
+    def test_join(self):
+        """
+        Make sure that when we get a socket connection we create a player
+        for that socket.
+        """
+        
+        self.namespace.flush()
+        
+        old_count = Player.objects.all().count()
+        self.assertTrue(self.namespace.on_join(str(self.game_room.pk)))
+        self.assertEqual(Player.objects.all().count(), old_count + 1)
+        self.namespace.runner.add_player.assert_called()
+
+        # Make sure we can't join a full room
+        self.assertFalse(self.namespace.on_join(str(self.game_room.pk)))
+        self.assertEqual(Player.objects.all().count(), old_count + 1)
+        self.namespace.emit.assert_called_with("error",
+                                               "Unable to join game room.")
+
+        # Make sure we can't join a bad room id
+        self.assertFalse(self.namespace.on_join("0"))
+        self.assertEqual(Player.objects.all().count(), old_count + 1)
+        self.namespace.emit.assert_called_with("error",
+                                               "Can not find game room")
+
+        # Make sure we can't join a bad room id
+        self.assertFalse(self.namespace.on_join("foo"))
+        self.assertEqual(Player.objects.all().count(), old_count + 1)
+        self.namespace.emit.assert_called_with("error",
+                                               "Room id is not an integer.")
+        
+        
 
     @skip("Not yet implemented")
     def test_invalid_move(self):
@@ -125,30 +164,23 @@ class GameNamespaceTestCase(SocketTestCase):
         self.namespace.on_action(valid_move)
         self.namespace.broadcast_event.assert_called_with("state_transition",
                                                           transitions)
-
-    @skip("Not yet implemented")
+        
     def test_request_state(self):
         """
         If we request the state, we should recieve the state back.
         """
 
         state = {'foo': 'bar'}
-
-        self.namespace.on_request_state()
+        
+        # Mock out the runner
+        runner = self.namespace.runner
+        runner.get_state = MagicMock()
+        runner.get_state.return_value = state
+        
+        self.namespace.on_request_state({})
         self.namespace.emit.assert_called_with("state", state)
-
-    @skip("Not yet implemented")
-    def test_connect(self):
-        """
-        Make sure that when we get a socket connection we create a player
-        for that socket.
-        """
-
-        old_count = Player.objects.all().count()
-        self.namespace.on_connect()
-        self.assertEqual(Player.objects.all().count(), old_count + 1)
-
-    @skip("Not yet implemented")
+        runner.get_state.assert_called_with(self.game_room.room_id)
+        
     def test_disconnect(self):
         """
         If a player disconnects from the room we should clean up the player
@@ -156,9 +188,11 @@ class GameNamespaceTestCase(SocketTestCase):
         """
 
         old_count = Player.objects.all().count()
+        self.namespace.recv_disconnect()
+        self.assertEqual(Player.objects.all().count(), old_count - 1)
+        
+        # Make sure we reconnect.
         self.namespace.player = Player.objects.create(
-            game_id=0,
+            game_room=self.game_room,
+            player_id = 0,
             nickname="Bob")
-
-        self.namespace.on_disconnect()
-        self.assertEqual(Player.objects.all().count(), old_count)
