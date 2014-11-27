83c3353630b801170418e8caecec5bda008fd6da
Alexander Bake <abake@uchicago.edu>
2014-11-26 23:49:50 -0600

    Fixed firefox hover image issue

--- webapp/game_defs/dominion/game.js
+++ webapp/game_defs/dominion/game.js
@@ -41,6 +41,18 @@ function specialEventBoxCardText(card_name) {
     }[card_name];
 }
 
+function setupHoverImages(card) {
+    var hover_id, src, big_src, img;
+    if (!$("#" + card.id).data("face_up")) return;
+    hover_id = card.id + "-hover";
+    // Create big image
+    src = card.src;
+    big_src = card.src.substring(0, src.length-4) + "-big.jpg";
+    img = '<img id="'+hover_id+'" class="hover" src="'+big_src+'" />';
+    $('body').append(img);
+    $('#'+hover_id).hide();
+}
+
 function setupAltText(cardData) {
     var card = $("#card" + cardData.game_id);
     var alt = "";
@@ -201,11 +213,6 @@ function supplyOnHover(e) {
     var img, src, hover_id;
     if (!$("#" + this.id).data("face_up")) return;
     hover_id = this.id + "-hover";
-    // Create big image
-    src = this.src;
-    big_src = this.src.substring(0, src.length-4) + "-big.jpg";
-    img = '<img id="'+hover_id+'" class="hover" src="'+big_src+'" />';
-    $('body').append(img);
     $("#"+hover_id)
         .css("top", (e.pageY + mouse_offset) + "px")
         .css("left", (e.pageX + mouse_offset) + "px")
@@ -225,7 +232,7 @@ function supplyOnMouseMove(e) {
 }
 
 function supplyOnMouseOut(e) {
-    $("#"+this.id+"-hover").remove();
+    $("#"+this.id+"-hover").hide();
 }
 
 function showHidePlayersOnClick() {
@@ -243,6 +250,7 @@ function supplyOnClick() {
 }
 
 function cardOnClick() {
+    $(".hover").hide();
     if (!expecting_select) {
         if (!$(this).parent().hasClass("supply")) {
             socket.emit('action', {
@@ -296,6 +304,7 @@ socket.on('state', function(data) {
     };
     setupInitialState(data);
     _.each(data.cards, setupAltText);
+    _.each($("img.card"), setupHoverImages);
     setupClickEvents(click_fn_map); 
     addBtn('Send Info', 'send-info-btn', sendInfoOnClick);
     addBtn('Next Phase', 'next-phase-btn', nextPhaseOnClick);
