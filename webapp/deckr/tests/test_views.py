01871b47c8d960cf3fb15bb7771332426214b7d1
Tristan Rasmussen <tristanr93@gmail.com>
2014-12-02 21:56:41 -0600

    Pylint.

--- webapp/deckr/tests/test_views.py
+++ webapp/deckr/tests/test_views.py
@@ -218,14 +218,11 @@ class UploadGameDefTestCase(TestCase):
         response = self.client.get(reverse('deckr.upload_game_definition'))
         self.assertEqual(response.status_code, 200)
 
-
     @override_settings(GAME_DEFINITION_PATH=tempfile.mkdtemp())
     def test_can_upload(self):
         """
         Make sure the form submits or displays validation
         """
-
-
 
         old_count = GameDefinition.objects.all().count()
         form_data = {'game_name': 'Solitaire',
