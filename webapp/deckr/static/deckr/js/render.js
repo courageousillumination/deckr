34b833f84a45fe899bb9dae321572000208d4f21
Alexander Bake <abake@uchicago.edu>
2014-12-02 18:26:49 -0600

    Fixed refactoring bug.

--- webapp/deckr/static/deckr/js/render.js
+++ webapp/deckr/static/deckr/js/render.js
@@ -2,10 +2,12 @@
 
 function addDiv(parentId, divDict, place) {
     var newDiv = document.createElement('div');
+    var parent = document.getElementById(parentId);
+    var siblings = parent.childNodes;
 
     if (!divDict["id"])
         return logAndReturnMessage("No id attr provided with div.");
-    if (document.getElementById(divDict["id"])) {
+    if (document.getElementById(divDict["id"]))
         return logAndReturnMessage("Duplicate div. Div already exists.");
 
     _.each(_.pairs(divDict), function(kv) {
@@ -13,6 +15,18 @@ function addDiv(parentId, divDict, place) {
         var v = kv[1];
         $(newDiv).attr(k, v);
     });
+
+    if (!place) {
+        unselectAll();
+        parent.appendChild(newDiv);
+    } else {
+        if (place < siblings.length) {
+            unselectAll();
+            toZone.insertBefore(newDiv, siblings[place]);
+        } else {
+            return logAndReturnMessage("Place does not exist.");
+        }
+    }
 }
 
 function removeElementById(id) {
