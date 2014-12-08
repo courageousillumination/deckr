a1afc6390fc9cedb9236bbc2aa582aa0c4c6fbd6
Tristan Rasmussen <tristanisthebest@gmail.com>
2014-12-07 19:50:02 -0600

    Added simple phase passing.

--- webapp/game_defs/magic/magic.py
+++ webapp/game_defs/magic/magic.py
@@ -3,9 +3,28 @@
 This module provides an implementation of Magic: The Gathering.
 """
 
-from engine.game import Game
+from engine.game import action, Game
+from engine.stateful_game_object import StatefulGameObject
 
 STARTING_HAND_SIZE = 7
+PHASE_ORDER = [
+    ("beginning", "untap"),
+    ("beginning", "upkeep"),
+    ("beginning", "draw"),
+
+    ("precombat_main", None),
+
+    ("combat", "beginning_of_combat"),
+    ("combat", "declare_attackers"),
+    ("combat", "declare_blockers"),
+    ("combat", "deal_combat_damage"),
+    ("combat", "end_of_combat"),
+
+    ("postcombat_main", None),
+
+    ("end", "end"),
+    ("end", "cleanup")
+]
 
 class Magic(Game):
     """
@@ -16,6 +35,12 @@ class Magic(Game):
         super(Magic, self).__init__()
         self.deck1 = [("Island", 30)]
         self.deck2 = [("Forest", 30)]
+
+        self.step = None
+        self.phase = None
+        self.active_player = None
+        self.has_priority_player = None
+        self.attacking = False
 
     ##################
     # Base functions #
@@ -37,17 +62,78 @@ class Magic(Game):
         for player in self.players:
             for _ in range(STARTING_HAND_SIZE):
                 self.draw_card(player)
+
             player.life = 20
 
+        self.phase = "beginning"
+        self.step = "upkeep"
+        self.active_player = self.players[0]
+        self.has_priority_player = self.players[0]
+
     def is_over(self):
-        pass
+        return False # TODO
 
     def winners(self):
-        pass
+        return [] # TODO
+
+    ###########
+    # Actions #
+    ###########
+
+    def has_priority(self, player):
+        return self.has_priority_player == player
+
+    @action(restriction = has_priority)
+    def pass_priority(self, player):
+        """
+        Pass the priority on to the next player.
+        """
+
+        next_player = self.get_next_player(player)
+        if (next_player == self.active_player and
+            self.stack.get_num_cards() == 0):
+            self.next_phase_or_step()
+        else:
+            self.has_priority_player = next_player
 
     ###############
     # Other stuff #
     ###############
+
+    def next_phase_or_step(self):
+        """
+        Move on to the next phase or step.
+        """
+
+        index = PHASE_ORDER.index((self.phase, self.step))
+
+        # TODO: This should probably use a game_step or something so we can
+        # insert triggers (when that's working....)
+        
+        # Deal with edge cases
+        if self.step == "declare_attackers" and not self.attacking:
+            self.step = "end_of_combat"
+        elif self.step == "cleanup":
+            # Change active player here
+            pass
+        else:
+            next_phase, next_step = PHASE_ORDER[index + 1]
+            self.step = next_step
+            self.phase = next_phase
+
+        self.has_priority_player = self.active_player
+
+    def get_next_player(self, player):
+        """
+        Gets the next player after a specific player in the turn order.
+        """
+
+        player_index = self.players.index(player)
+        if player_index == (len(self.players) - 1):
+            return self.players[0]
+        else:
+            return self.players[player_index + 1]
+
 
     def move_card(self, card, target_zone):
         # Account for visibility here
--- webapp/game_defs/magic/test.py
+++ webapp/game_defs/magic/test.py
@@ -35,3 +35,71 @@ class MagicTestCase(TestCase):
         # Make sure player attributes were initalized correctly
         self.assertEqual(self.game.players[0].life, 20)
         self.assertEqual(self.game.players[1].life, 20)
+
+    def test_pass_priority(self):
+        """
+        Make sure we can pass priority from one player to another and that
+        the phase or step update accordingly.
+        """
+
+        # To begin with we should be in the beginning phase, upkeep step
+        self.game.step = "upkeep"
+        self.game.phase = "beginning"
+        self.game.active_player = self.player1
+        self.game.has_priority_player = self.player1
+
+        # Now we pass twice and we should be in the draw step
+        self.game.pass_priority(self.player1)
+        self.game.pass_priority(self.player2)
+
+        self.assertEqual(self.game.step, "draw")
+        self.assertEqual(self.game.phase, "beginning")
+
+        # Now when we pass priority here we should be in precombat main phase
+        self.game.pass_priority(self.player1)
+        self.game.pass_priority(self.player2)
+
+        self.assertEqual(self.game.step, None)
+        self.assertEqual(self.game.phase, "precombat_main")
+
+        # Pass again into the combat phase
+        self.game.pass_priority(self.player1)
+        self.game.pass_priority(self.player2)
+
+        self.assertEqual(self.game.step, "beginning_of_combat")
+        self.assertEqual(self.game.phase, "combat")
+
+        # Now we move on to declare attackers
+        self.game.pass_priority(self.player1)
+        self.game.pass_priority(self.player2)
+
+        self.assertEqual(self.game.step, "declare_attackers")
+        self.assertEqual(self.game.phase, "combat")
+
+        # Nobody was declared as attacking so we skip right to the end of combat
+        self.game.pass_priority(self.player1)
+        self.game.pass_priority(self.player2)
+
+        self.assertEqual(self.game.step, "end_of_combat")
+        self.assertEqual(self.game.phase, "combat")
+
+        # Next we move on to the second main phase
+        self.game.pass_priority(self.player1)
+        self.game.pass_priority(self.player2)
+
+        self.assertEqual(self.game.step, None)
+        self.assertEqual(self.game.phase, "postcombat_main")
+
+        # Finally we go to the end phase
+        self.game.pass_priority(self.player1)
+        self.game.pass_priority(self.player2)
+
+        self.assertEqual(self.game.step, "end")
+        self.assertEqual(self.game.phase, "end")
+
+        # Clean up phase
+        self.game.pass_priority(self.player1)
+        self.game.pass_priority(self.player2)
+
+        self.assertEqual(self.game.step, "cleanup")
+        self.assertEqual(self.game.phase, "end")
