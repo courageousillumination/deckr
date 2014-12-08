b931645009e3f00b2ce0b5a20ca38716fd48e5d2
Tristan Rasmussen <tristanisthebest@gmail.com>
2014-12-07 18:49:45 -0600

    Added initial set up for magic.

--- webapp/game_defs/magic/magic.py
+++ webapp/game_defs/magic/magic.py
@@ -5,6 +5,7 @@ This module provides an implementation of Magic: The Gathering.
 
 from engine.game import Game
 
+STARTING_HAND_SIZE = 7
 
 class Magic(Game):
     """
@@ -13,17 +14,44 @@ class Magic(Game):
 
     def __init__(self):
         super(Magic, self).__init__()
-
+        self.deck1 = [("Island", 30)]
+        self.deck2 = [("Forest", 30)]
 
     ##################
     # Base functions #
     ##################
 
     def set_up(self):
-        pass
+        decks_and_players = [(self.deck1, self.players[0]),
+                             (self.deck2, self.players[1])]
+
+        for deck_list, player in decks_and_players:
+            deck = []
+            for card, num in deck_list:
+                deck += self.card_set.create(card, num)
+
+            self.register(deck)
+            player.library.set_cards(deck)
+
+        # Perform actual set up
+        for player in self.players:
+            for _ in range(STARTING_HAND_SIZE):
+                self.draw_card(player)
+            player.life = 20
 
     def is_over(self):
         pass
 
     def winners(self):
         pass
+
+    ###############
+    # Other stuff #
+    ###############
+
+    def move_card(self, card, target_zone):
+        # Account for visibility here
+        target_zone.push(card)
+
+    def draw_card(self, player):
+        self.move_card(player.library.pop(), player.hand)
--- webapp/game_defs/magic/test.py
+++ webapp/game_defs/magic/test.py
@@ -6,9 +6,32 @@ from engine.game_runner import load_game_definition
 class MagicTestCase(TestCase):
 
     def setUp(self):
-        self.game, config = load_game_definition("game_defs/dominion")
+        self.game, config = load_game_definition("game_defs/magic")
         self.game.load_config(config)
         self.player1 = self.game.get_object_with_id("Player",
                                                     self.game.add_player())
         self.player2 = self.game.get_object_with_id("Player",
                                                     self.game.add_player())
+
+    def test_set_up(self):
+        deck1 = [("Forest", 30)]
+        deck2 = [("Island", 30)]
+
+        self.game.deck1 = deck1
+        self.game.deck2 = deck2
+
+        self.game.set_up()
+
+        # Make sure both players have the right cards in hand and library
+        self.assertEqual(self.game.players[0].library.get_num_cards(), 23)
+        self.assertEqual(self.game.players[1].library.get_num_cards(), 23)
+
+        self.assertEqual(self.game.players[0].hand.get_num_cards(), 7)
+        self.assertEqual(self.game.players[1].hand.get_num_cards(), 7)
+
+        self.assertEqual(self.game.players[0].library.peek().name, "Forest")
+        self.assertEqual(self.game.players[1].library.peek().name, "Island")
+
+        # Make sure player attributes were initalized correctly
+        self.assertEqual(self.game.players[0].life, 20)
+        self.assertEqual(self.game.players[1].life, 20)
